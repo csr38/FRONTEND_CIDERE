203826ae539a032905e2fc0a986fef5012629f30
csr38 <cesar1tapia1@gmail.com>
2023-11-04 17:07:14 -0300

    Se cre la pagina de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,233 @@
-import { Button, Collapse, Layout, Switch, theme } from 'antd'
-import React, { useState } from 'react'
-import Logo from './components/logo'
-import MenuList  from './components/MenuList'
-import ToggleThemeButton from './components/ToggleThemeButton'
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
-import { Routes, Route } from 'react-router-dom'
+import { Button, Collapse, Layout, Switch, theme } from "antd";
+import React, { useState } from "react";
+import Logo from "./components/logo";
+import MenuList from "./components/MenuList";
+import ToggleThemeButton from "./components/ToggleThemeButton";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import { Routes, Route } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
 
 
-const { Header, Sider } = Layout
+
+
+import  IndexPage  from "./pages/IndexPage";
+
+const { Header, Sider } = Layout;
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(true)
-  const toggleTheme = () => {setDarkTheme(!darkTheme)}
-  const [collapsed, setCollapsed] = useState(false)
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const { token: {colorBgContainer} } = theme.useToken()
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  const [darkTheme, setDarkTheme] = useState(true);
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
     <>
-
       <Layout>
-          <Sider 
-            collapsed={collapsed}
-            collapsible
-            trigger={null}
-            theme={darkTheme? 'dark' : 'light'} className='sidebar'>
-              <Logo />
-              <MenuList darkTheme = { darkTheme }/>
-              <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
-          </Sider>
-          <Layout>
-              <Header style={{padding: 0, background:colorBgContainer}} >
-                  <Button 
-                    type="text" 
-                    className='toggle'
-                    onClick={() => setCollapsed(!collapsed)}
-                    icon={ collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />} />
-              </Header>
-            </Layout>
+        <Sider
+          collapsed={collapsed}
+          collapsible
+          trigger={null}
+          theme={darkTheme ? "dark" : "light"}
+          className="sidebar"
+        >
+          <Logo />
+          <MenuList darkTheme={darkTheme} />
+          <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
+        </Sider>
 
-            <Routes>
-              <Route path="/" element={<div>Home  uno</div>} />
-              <Route path="/activity" element={<div>Activity</div>} />
-              <Route path="/tasks" element={<div>Tasks</div>} />
-              <Route path="/progress" element={<div>Progress</div>} />
-              <Route path="/payment" element={<div>Payment</div>} />
-              <Route path="/setting" element={<div>Setting</div>} />
-            </Routes>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar
+            position="static"
+            style={{ paddingLeft: 0, background: colorBgContainer }}
+          >
+            <Toolbar style={{ paddingLeft: 0, margin: 0 }}>
+              <Button
+                style={{ margin: 0 }}
+                type="text"
+                className="toggle"
+                onClick={() => setCollapsed(!collapsed)}
+                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              />
+
+              <Box sx={{ flexGrow: 1 }} />
+              <Box sx={{ display: { xs: "none", md: "flex" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="show 4 new mails"
+                  color="primary"
+                >
+                  <Badge badgeContent={4} color="error">
+                    <MailIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  aria-label="show 17 new notifications"
+                  color="secondary"
+                >
+                  <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="secondary"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </Box>
+
+              <Box sx={{ display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="show more"
+                  aria-controls={mobileMenuId}
+                  aria-haspopup="true"
+                  onClick={handleMobileMenuOpen}
+                  color="inherit"
+                >
+                  <MoreIcon />
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </AppBar>
+          {renderMobileMenu}
+          {renderMenu}
+
+          <Routes>
+            <Route path="/" element={ <IndexPage/> } />
+            <Route path="/activity" element={<div>Activity</div>} />
+            <Route path="/tasks" element={<div>Tasks</div>} />
+            <Route path="/progress" element={<div>Progress</div>} />
+            <Route path="/payment" element={<div>Payment</div>} />
+            <Route path="/setting" element={<div>Setting</div>} />
+          </Routes>
+          
+
+        </Box>
+
+        
       </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
